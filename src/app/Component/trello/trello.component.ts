729c6cb724b9d6d6d8ddc91bcb2018b0fd5355f2
Mohammed Ziyad <94079392+Mohammed-Ziyad@users.noreply.github.com>
2024-03-18 11:19:14 +0300

    edit in add

--- src/app/Component/trello/trello.component.ts
+++ src/app/Component/trello/trello.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup , Validators } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
@@ -13,34 +13,56 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
+import { CdkTextareaAutosize, TextFieldModule } from '@angular/cdk/text-field';
+import { NgClass } from '@angular/common';
+import { ScrumboardBoardAddCardComponent } from "../add-card/add-card.component";
 @Component({
-  selector: 'app-trello',
-  standalone: true,
-  imports: [
-    MatCardModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    CdkDropList,
-    CdkDrag,],
-  templateUrl: './trello.component.html',
-  styleUrl: './trello.component.scss'
+    selector: 'app-trello',
+    standalone: true,
+    templateUrl: './trello.component.html',
+    styleUrl: './trello.component.scss',
+    imports: [
+        MatCardModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        CdkDropList,
+        CdkDrag, MatButtonModule, NgClass, FormsModule, ReactiveFormsModule, TextFieldModule,
+        ScrumboardBoardAddCardComponent
+    ]
 })
-export class TrelloComponent {
+export class TrelloComponent implements OnInit {
   todoForm! : FormGroup;
   tasks: Task[] = [];
   inprogress:Task[] = [];
   done:Task[] = [];
+  @ViewChild('titleInput') titleInput!: ElementRef ;
+  @ViewChild('titleAutosize') titleAutosize!: CdkTextareaAutosize ;
+  @Input() buttonTitle: string = 'Add a card';
+  @Output() readonly saved: EventEmitter<string> = new EventEmitter<string>();
 
+  form!: UntypedFormGroup ;
+  formVisible: boolean = false;
+
+  isadd: boolean = false;
   isEditEnabled: boolean = false;
   updatedIndex!:any;
-  constructor(private fb:FormBuilder){}
+  constructor(private fb:FormBuilder ,
+    private _changeDetectorRef: ChangeDetectorRef,
+    private _formBuilder: UntypedFormBuilder,
+
+
+    ){}
     ngOnInit(): void {
       this.todoForm = this.fb.group({
-        item : ['', [Validators.required, Validators.minLength(3)]]
+        item : ['', [Validators.required]]
       });
+      // Initialize the new list form
+      this.form = this._formBuilder.group({
+        title: [''],
+    });
     }
 
     addTask()
@@ -50,6 +72,18 @@ export class TrelloComponent {
         Completed:false
       });
       this.todoForm.reset();
+    }
+    addTaskformCard()
+    {
+      if(this.isadd==true)
+      {
+
+         this.addTask()
+         this.isadd=!this.isadd
+         return
+        }
+        this.isadd=!this.isadd
+
     }
     updateTask()
     {
@@ -88,5 +122,65 @@ export class TrelloComponent {
         );
       }
     }
+
+    /**
+     * Save
+     */
+    save(): void
+    {
+        // Get the new list title
+        const title = this.form.get('title')?.value;
+
+        // Return, if the title is empty
+        if ( !title || title.trim() === '' )
+        {
+            return;
+        }
+
+        // Execute the observable
+        this.saved.next(title.trim());
+
+        // Clear the new list title and hide the form
+        this.formVisible = false;
+        this.form.get('title')?.setValue('');
+
+        // Reset the size of the textarea
+        setTimeout(() =>
+        {
+            this.titleInput.nativeElement.value = '';
+            this.titleAutosize.reset();
+        });
+
+        // Mark for check
+        this._changeDetectorRef.markForCheck();
+    }
+
+    /**
+     * Toggle the visibility of the form
+     */
+    toggleFormVisibility(): void
+    {
+        // Toggle the visibility
+        this.formVisible = !this.formVisible;
+
+        // If the form becomes visible, focus on the title field
+        if ( this.formVisible )
+        {
+            this.titleInput.nativeElement.focus();
+        }
+    }
+
+    addCard(list: Task, title: string): void
+    {
+        // Create a new card model
+
+        this.tasks.push({
+          Title : this.todoForm.value.item,
+          Completed:false
+        });
+        // Save the card
+        // this._scrumboardService.createCard(card).subscribe();
+    }
+
 
 }
